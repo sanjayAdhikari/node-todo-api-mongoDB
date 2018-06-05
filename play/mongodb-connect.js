180a60dfc112dc8354e9551990a299325d97d108
Sanjay Adhikari <idsanjay81@gmail.com>
2018-06-05 08:11:39 +0545

    Update mongodb-connect.js

--- play/mongodb-connect.js
+++ play/mongodb-connect.js
@@ -11,22 +11,22 @@ MongoClient.connect(dbURL, (err, DB) => {
     console.log('Connected to Mongodb server');
     var db = DB.db('TodoApp');
 
-    // db.collection('Todos').insertOne({
-    //     text: 'nothing to do',
-    //     completed: true
-    // }, (err, result) => {
-    //     if (err) return console.log('Unable to insert Todos', err);
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
+    db.collection('Todos').insertOne({
+        text: 'nothing to do',
+        completed: true
+    }, (err, result) => {
+        if (err) return console.log('Unable to insert Todos', err);
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
 
-    // db.collection('Users').insertOne({
-    //     name: 'Sanjay',
-    //     age: 19,
-    //     location: 'Khursanitar-35,Lazimpat, Kathmandu'
-    // }, (err, results) => {
-    //     if(err) return console.log('Unable to inset User', err);
-    //     console.log(JSON.stringify(results.ops,undefined,2));
-    // })
+    db.collection('Users').insertOne({
+        name: 'Sanjay',
+        age: 19,
+        location: 'Khursanitar-35,Lazimpat, Kathmandu'
+    }, (err, results) => {
+        if(err) return console.log('Unable to inset User', err);
+        console.log(JSON.stringify(results.ops,undefined,2));
+    })
     
     DB.close();
 })
